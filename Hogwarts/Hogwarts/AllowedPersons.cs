dce501f3a5461c5b633e49f670cb4c76f93d77e0
taha-najaf <127234497+taha-najaf@users.noreply.github.com>
2023-05-12 22:08:15 +0330

    Allowed person class add

---
+++ Hogwarts/Hogwarts/AllowedPersons.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hogwarts
+{
+    public class AllowedPersons
+    {
+        public string Curriculum { get; set; }
+        public Pet Pet { get; set; }
+        public GroupType GroupName { get; set; }
+        public bool HaveBaggage { get; set; }
+        public Role Role { get; set; }
+        public string ReceivedLetter = " ";
+    }
+}
