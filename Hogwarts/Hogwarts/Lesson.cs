9df31b6c5c4b30ab42f2a816c9451025434f98a5
taha-najaf <127234497+taha-najaf@users.noreply.github.com>
2023-05-12 22:18:49 +0330

    Lesson class add

---
+++ Hogwarts/Hogwarts/Lesson.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hogwarts
+{
+    public class Lesson
+    {
+        public string ClassTime { get; set; }
+        public int NumberOfStudents { get; set; }
+        public int Capacity { get; set; }
+        public int PresentationSemester { get; set; }
+        public string Name { get; set; }
+        public string Professor { get; set; }
+        public double StudentGrade { get; set; }
+    }
+}
