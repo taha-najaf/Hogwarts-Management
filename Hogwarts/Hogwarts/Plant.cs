b31361ace70b7896d46800d2e4781ba9b28404f8
taha-najaf <127234497+taha-najaf@users.noreply.github.com>
2023-05-12 22:26:22 +0330

    Teacher class add

---
+++ Hogwarts/Hogwarts/Plant.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hogwarts
+{
+    public class Plant
+    {
+        public string Name { get; set; }
+        public int Number { get; set; }
+    }
+}
---
+++ Hogwarts/Hogwarts/Teacher.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hogwarts
+{
+    public class Teacher : AllowedPersons
+    {
+        public bool SimultaneousTeaching { get; set; }
+        public Lesson Lesson { get; set; }
+        public void DefineLesson(List<Lesson> lessons, List<Teacher> professor)
+        {
+        }
+        public void SetScor(List<Student> students)
+        {
+        }
+    }
+}
