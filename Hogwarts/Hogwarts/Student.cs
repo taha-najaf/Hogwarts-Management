80b89496cc0c47f1ae309e41fc34a1b6a2e98f34
taha-najaf <127234497+taha-najaf@users.noreply.github.com>
2023-05-12 22:30:44 +0330

    Student class add

---
+++ Hogwarts/Hogwarts/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hogwarts
+{
+    public class Student : AllowedPersons
+    {
+        public List<string> PassedUnits { get; set; }
+        public int Term { get; set; }
+        public int DormitoryNumbere { get; set; }
+        public string letterToDumbledor { get; set; }
+        public void SendLetterToDumbledor(string name, string family, string father, List<Student> student)
+        {
+        }
+        public void SelectUnits(List<Lesson> lessons, List<Student> students, string name, String family)
+        {
+        }
+
+    }
+}
