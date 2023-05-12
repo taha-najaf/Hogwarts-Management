3b5101482711f1002e74015cf02ab6ec6210e03d
taha-najaf <127234497+taha-najaf@users.noreply.github.com>
2023-05-12 22:35:18 +0330

    Dumbledore(Admin) class add

---
+++ Hogwarts/Hogwarts/Dumbledore.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hogwarts
+{
+    public class Dumbledore : AllowedPersons
+    {
+        Dormitory DormitoryList;
+        public List<string> letterFromStudents { get; set; }
+        public static void SendLetterToStudents(List<Student> persons)//need cabin and sit number
+        {
+        }
+        public static void Gardening(Plant plant)
+        {
+        }
+        public void AnswerLetters(List<Student> students)
+        {
+        }
+    }
+}
