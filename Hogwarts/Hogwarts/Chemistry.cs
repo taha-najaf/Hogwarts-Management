8cca07b923b69a180cfb23623a4dbcaff1a16fef
taha-najaf <127234497+taha-najaf@users.noreply.github.com>
2023-05-12 22:12:31 +0330

    Chemistry class add

---
+++ Hogwarts/Hogwarts/Chemistry.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hogwarts
+{
+    public class Chemistry
+    {
+        public List<string> ListOfChemicals { get; set; }
+    }
+}
