2651f59c04c1ef989f26266b16bcf722402d1124
taha-najaf <127234497+taha-najaf@users.noreply.github.com>
2023-05-17 21:01:17 +0330

    Dormitory Info Form add

---
+++ Hogwarts/Hogwarts/DormitoryInfoForm.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace Hogwarts
+{
+    partial class DormitoryInfoForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DormitoryInfoForm));
+            this.CoursesButton = new System.Windows.Forms.Button();
+            this.StudentButton = new System.Windows.Forms.Button();
+            this.NameSchoolLabel = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.LoginoutButton = new System.Windows.Forms.Button();
+            this.TrainButton = new System.Windows.Forms.Button();
+            this.ProfessorButton = new System.Windows.Forms.Button();
+            this.CoursesPictureBox = new System.Windows.Forms.PictureBox();
+            this.StudentPictureBox = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.LogoutPictureBox = new System.Windows.Forms.PictureBox();
+            this.TrainPictureBox = new System.Windows.Forms.PictureBox();
+            this.ProfessorPictureBox = new System.Windows.Forms.PictureBox();
+            this.ForestButton = new System.Windows.Forms.Button();
+            this.JunglePictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.CoursesPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.StudentPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LogoutPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TrainPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ProfessorPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.JunglePictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CoursesButton
+            // 
+            this.CoursesButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.CoursesButton.FlatAppearance.BorderSize = 5;
+            this.CoursesButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LemonChiffon;
+            this.CoursesButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CoursesButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CoursesButton.Location = new System.Drawing.Point(85, 448);
+            this.CoursesButton.Name = "CoursesButton";
+            this.CoursesButton.Size = new System.Drawing.Size(93, 48);
+            this.CoursesButton.TabIndex = 85;
+            this.CoursesButton.Text = "Courses";
+            this.CoursesButton.UseVisualStyleBackColor = true;
+            // 
+            // StudentButton
+            // 
+            this.StudentButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.StudentButton.FlatAppearance.BorderSize = 5;
+            this.StudentButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LemonChiffon;
+            this.StudentButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.StudentButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StudentButton.Location = new System.Drawing.Point(85, 284);
+            this.StudentButton.Name = "StudentButton";
+            this.StudentButton.Size = new System.Drawing.Size(93, 45);
+            this.StudentButton.TabIndex = 83;
+            this.StudentButton.Text = "Students";
+            this.StudentButton.UseVisualStyleBackColor = true;
+            // 
+            // NameSchoolLabel
+            // 
+            this.NameSchoolLabel.BackColor = System.Drawing.Color.Transparent;
+            this.NameSchoolLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NameSchoolLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.NameSchoolLabel.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.NameSchoolLabel.Location = new System.Drawing.Point(293, 5);
+            this.NameSchoolLabel.Name = "NameSchoolLabel";
+            this.NameSchoolLabel.Size = new System.Drawing.Size(792, 48);
+            this.NameSchoolLabel.TabIndex = 80;
+            this.NameSchoolLabel.Text = "Hogwarts School of Witchcraft and Wizardry";
+            this.NameSchoolLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Khaki;
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.ForeColor = System.Drawing.Color.Khaki;
+            this.panel3.Location = new System.Drawing.Point(184, -3);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(10, 671);
+            this.panel3.TabIndex = 71;
+            // 
+            // LoginoutButton
+            // 
+            this.LoginoutButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.LoginoutButton.FlatAppearance.BorderSize = 5;
+            this.LoginoutButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LemonChiffon;
+            this.LoginoutButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.LoginoutButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoginoutButton.Location = new System.Drawing.Point(85, 617);
+            this.LoginoutButton.Name = "LoginoutButton";
+            this.LoginoutButton.Size = new System.Drawing.Size(93, 45);
+            this.LoginoutButton.TabIndex = 76;
+            this.LoginoutButton.Text = "Logout";
+            this.LoginoutButton.UseVisualStyleBackColor = true;
+            this.LoginoutButton.Click += new System.EventHandler(this.LoginoutButton_Click);
+            // 
+            // TrainButton
+            // 
+            this.TrainButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.TrainButton.FlatAppearance.BorderSize = 5;
+            this.TrainButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LemonChiffon;
+            this.TrainButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.TrainButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TrainButton.Location = new System.Drawing.Point(85, 360);
+            this.TrainButton.Name = "TrainButton";
+            this.TrainButton.Size = new System.Drawing.Size(93, 48);
+            this.TrainButton.TabIndex = 77;
+            this.TrainButton.Text = "Train";
+            this.TrainButton.UseVisualStyleBackColor = true;
+            // 
+            // ProfessorButton
+            // 
+            this.ProfessorButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.ProfessorButton.FlatAppearance.BorderSize = 5;
+            this.ProfessorButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LemonChiffon;
+            this.ProfessorButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ProfessorButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ProfessorButton.Location = new System.Drawing.Point(85, 194);
+            this.ProfessorButton.Name = "ProfessorButton";
+            this.ProfessorButton.Size = new System.Drawing.Size(93, 44);
+            this.ProfessorButton.TabIndex = 79;
+            this.ProfessorButton.Text = "Professor";
+            this.ProfessorButton.UseVisualStyleBackColor = true;
+            // 
+            // CoursesPictureBox
+            // 
+            this.CoursesPictureBox.BackColor = System.Drawing.Color.Khaki;
+            this.CoursesPictureBox.Image = global::Hogwarts.Properties.Resources.download__3_;
+            this.CoursesPictureBox.Location = new System.Drawing.Point(3, 424);
+            this.CoursesPictureBox.Name = "CoursesPictureBox";
+            this.CoursesPictureBox.Padding = new System.Windows.Forms.Padding(5, 0, 0, 0);
+            this.CoursesPictureBox.Size = new System.Drawing.Size(76, 72);
+            this.CoursesPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.CoursesPictureBox.TabIndex = 84;
+            this.CoursesPictureBox.TabStop = false;
+            // 
+            // StudentPictureBox
+            // 
+            this.StudentPictureBox.BackColor = System.Drawing.Color.Khaki;
+            this.StudentPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.StudentPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("StudentPictureBox.Image")));
+            this.StudentPictureBox.Location = new System.Drawing.Point(3, 257);
+            this.StudentPictureBox.Name = "StudentPictureBox";
+            this.StudentPictureBox.Padding = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.StudentPictureBox.Size = new System.Drawing.Size(76, 72);
+            this.StudentPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.StudentPictureBox.TabIndex = 82;
+            this.StudentPictureBox.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Hogwarts.Properties.Resources.download1;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(175, 159);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 81;
+            this.pictureBox1.TabStop = false;
+            // 
+            // LogoutPictureBox
+            // 
+            this.LogoutPictureBox.BackColor = System.Drawing.Color.Khaki;
+            this.LogoutPictureBox.Image = global::Hogwarts.Properties.Resources.download__4_;
+            this.LogoutPictureBox.Location = new System.Drawing.Point(3, 597);
+            this.LogoutPictureBox.Name = "LogoutPictureBox";
+            this.LogoutPictureBox.Padding = new System.Windows.Forms.Padding(5, 7, 0, 0);
+            this.LogoutPictureBox.Size = new System.Drawing.Size(76, 65);
+            this.LogoutPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.LogoutPictureBox.TabIndex = 75;
+            this.LogoutPictureBox.TabStop = false;
+            // 
+            // TrainPictureBox
+            // 
+            this.TrainPictureBox.BackColor = System.Drawing.Color.Khaki;
+            this.TrainPictureBox.Image = global::Hogwarts.Properties.Resources.download__6_;
+            this.TrainPictureBox.Location = new System.Drawing.Point(3, 347);
+            this.TrainPictureBox.Name = "TrainPictureBox";
+            this.TrainPictureBox.Padding = new System.Windows.Forms.Padding(5, 3, 0, 0);
+            this.TrainPictureBox.Size = new System.Drawing.Size(76, 61);
+            this.TrainPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.TrainPictureBox.TabIndex = 73;
+            this.TrainPictureBox.TabStop = false;
+            // 
+            // ProfessorPictureBox
+            // 
+            this.ProfessorPictureBox.BackColor = System.Drawing.Color.Khaki;
+            this.ProfessorPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("ProfessorPictureBox.Image")));
+            this.ProfessorPictureBox.Location = new System.Drawing.Point(3, 166);
+            this.ProfessorPictureBox.Name = "ProfessorPictureBox";
+            this.ProfessorPictureBox.Padding = new System.Windows.Forms.Padding(5, 0, 0, 0);
+            this.ProfessorPictureBox.Size = new System.Drawing.Size(76, 72);
+            this.ProfessorPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.ProfessorPictureBox.TabIndex = 72;
+            this.ProfessorPictureBox.TabStop = false;
+            // 
+            // ForestButton
+            // 
+            this.ForestButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.ForestButton.FlatAppearance.BorderSize = 5;
+            this.ForestButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LemonChiffon;
+            this.ForestButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ForestButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForestButton.Location = new System.Drawing.Point(85, 530);
+            this.ForestButton.Margin = new System.Windows.Forms.Padding(0);
+            this.ForestButton.Name = "ForestButton";
+            this.ForestButton.Size = new System.Drawing.Size(93, 48);
+            this.ForestButton.TabIndex = 87;
+            this.ForestButton.Text = "Forest";
+            this.ForestButton.UseVisualStyleBackColor = true;
+            // 
+            // JunglePictureBox
+            // 
+            this.JunglePictureBox.BackColor = System.Drawing.Color.Khaki;
+            this.JunglePictureBox.Image = global::Hogwarts.Properties.Resources.download__5_;
+            this.JunglePictureBox.Location = new System.Drawing.Point(3, 513);
+            this.JunglePictureBox.Name = "JunglePictureBox";
+            this.JunglePictureBox.Padding = new System.Windows.Forms.Padding(5, 0, 0, 0);
+            this.JunglePictureBox.Size = new System.Drawing.Size(76, 65);
+            this.JunglePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.JunglePictureBox.TabIndex = 86;
+            this.JunglePictureBox.TabStop = false;
+            // 
+            // DormitoryInfoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(120)))), ((int)(((byte)(100)))));
+            this.ClientSize = new System.Drawing.Size(1262, 673);
+            this.Controls.Add(this.ForestButton);
+            this.Controls.Add(this.JunglePictureBox);
+            this.Controls.Add(this.CoursesButton);
+            this.Controls.Add(this.CoursesPictureBox);
+            this.Controls.Add(this.StudentButton);
+            this.Controls.Add(this.StudentPictureBox);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.NameSchoolLabel);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.LoginoutButton);
+            this.Controls.Add(this.TrainButton);
+            this.Controls.Add(this.ProfessorButton);
+            this.Controls.Add(this.LogoutPictureBox);
+            this.Controls.Add(this.TrainPictureBox);
+            this.Controls.Add(this.ProfessorPictureBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "DormitoryInfoForm";
+            this.Text = "DormitoryInfoForm";
+            ((System.ComponentModel.ISupportInitialize)(this.CoursesPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.StudentPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LogoutPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TrainPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ProfessorPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.JunglePictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CoursesButton;
+        private System.Windows.Forms.PictureBox CoursesPictureBox;
+        private System.Windows.Forms.Button StudentButton;
+        private System.Windows.Forms.PictureBox StudentPictureBox;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label NameSchoolLabel;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button LoginoutButton;
+        private System.Windows.Forms.Button TrainButton;
+        private System.Windows.Forms.Button ProfessorButton;
+        private System.Windows.Forms.PictureBox LogoutPictureBox;
+        private System.Windows.Forms.PictureBox TrainPictureBox;
+        private System.Windows.Forms.PictureBox ProfessorPictureBox;
+        private System.Windows.Forms.Button ForestButton;
+        private System.Windows.Forms.PictureBox JunglePictureBox;
+    }
+}
